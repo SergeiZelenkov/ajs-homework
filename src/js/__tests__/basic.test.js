aa5eaafcddf957a04e0e28d93f9bb61ee91fcee7
Sergei Zelenkov <sz.zelenkov@gmx.de>
2025-02-16 16:02:34 +0100

    init

---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,16 @@
+import healthLevel from '../level';
+
+test('level healthy', () => {
+  const result = healthLevel({ name: 'Маг', health: 90});
+  expect(result).toBe("healthy");
+});
+
+test('level wounded', () => {
+   const result = healthLevel({ name: 'лучник', health: 50});
+   expect(result).toBe("wounded");
+ });
+
+ test('level critical', () => {
+   const result = healthLevel({ name: 'мечник', health: 10});
+   expect(result).toBe("critical");
+ });
